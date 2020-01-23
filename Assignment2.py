5639f953b3e5c9bc7d69da77b4c291a6ff14ac34
Shukrithi <ced16i031@iiitdm.ac.in>
2020-01-23 18:00:19 +0530

    Updated ReadMe nd Assignment 2

--- Assignment2.py
+++ Assignment2.py
@@ -6,32 +6,36 @@ import matplotlib.pyplot as plt
 import skimage
 
 # Load in grayscale mode
-lena = cv2.imread('lena.jpeg', 0)
+lena = cv2.imread('lena.jpeg')
 
 #print matrix
-print(lena)
-
-plt.gray()
+print("Lena:",lena)
 plt.imshow(lena)
 plt.show()
 
-def plotnoise(lena, mode, r, c, i):
+lena_gray=cv2.imread('lena.jpeg',0)
+print(lena_gray)
+plt.gray()
+plt.imshow(lena_gray)
+plt.show()
+
+def plotnoise(lena_gray, mode, r, c, i):
     plt.subplot(r,c,i)
     if mode is not None:
-        gimg = skimage.util.random_noise(lena, mode=mode)
+        gimg = skimage.util.random_noise(lena_gray, mode=mode)
         plt.imshow(gimg)
     else:
-        plt.imshow(lena)
+        plt.imshow(lena_gray)
     plt.title(mode)
     plt.axis("off")
 
 plt.figure(figsize=(30,40))
 r=3
 c=2
-plotnoise(lena, None, r,c,1)
-plotnoise(lena, "gaussian", r,c,2)
-plotnoise(lena, "salt", r,c,3)
-plotnoise(lena, "pepper", r,c,4)
-plotnoise(lena, "s&p", r,c,5)
-plotnoise(lena, "speckle", r,c,6)
+plotnoise(lena_gray, None, r,c,1)
+plotnoise(lena_gray, "gaussian", r,c,2)
+plotnoise(lena_gray, "salt", r,c,3)
+plotnoise(lena_gray, "pepper", r,c,4)
+plotnoise(lena_gray, "s&p", r,c,5)
+plotnoise(lena_gray, "speckle", r,c,6)
 plt.show()
--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,20 @@
 # Digital Image Processing
+Digital Image Processing Coursework
 
-Analytics and Systems of Big Data Coursework
+#### Assignment 1
 
+1. Read a matrix of size 5*5 and find the following by using a user-defined function.
+    i. sum
+    ii. maximum
+    iii.mean  
+    iv.median
+    v.mode
+    vi.standard deviation
+    v. frequency distribution
+
+2. Read the matrix size through keyboard and create a random matrix of integers ranging from  0 to 10 and compute all the above functions listed in question 1. 
+
+#### Assignment 2
+
+Take a Lena image and convert it into grayscale. Add three different types of noises(salt and pepper, additive Gaussian noise, speckle), each noise in the sets of 5,10,15,20,25,30. Take average for each set and display the average images. 
 
