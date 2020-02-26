980cc9dbddbb41f6d2a0cbe92cf7eabb99d88237
Shukrithi <ced16i031@iiitdm.ac.in>
2020-02-26 23:05:09 +0530

    Assignment 5 -1 and 2D transform

---
+++ Q5_1FFT.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = np.random.random(10)
+
+def FFT(x):
+    F=[]
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    for U in range(0,N):
+        sum=0
+        for y in range(0,N):
+            sum=sum+(x[y]*(np.exp(-2j * np.pi * U * y / N)))
+        F.append(sum)
+    return(F)
+x = np.random.random(10)
+x = np.asarray(x)
+# N = x.shape[0]
+print(FFT(x))
+print(np.allclose(np.fft.fft(x),FFT(x)))
+
+
+    
